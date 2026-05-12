bac245c68e074f1015c48e0ffcaa682f2c6361c6
Adarsh Ranjan <adarsh.aur@gmail.com>
2026-05-12 21:29:46 +0530

    added local storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     });
 
     const html = `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -102,5 +102,8 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     link.addEventListener("click", () => {
         const { productId } = link.dataset;
         deleteProduct(productId);
+
+        const deleteElement = document.querySelector(`.js-cart-item-container-${productId}`);
+        deleteElement.remove();
     });
 });
