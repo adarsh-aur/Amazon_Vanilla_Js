a3a17ad6ee07b43391a4f76b252995018f5f9af9
Adarsh Ranjan <adarsh.aur@gmail.com>
2026-05-25 22:04:28 +0530

    add backend practice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "../scripts/checkout/checkoutHeader.js";
-// import "../data/cart-class.js"; 
-
+// import "../data/cart-class.js";
+import "../data/backend-practice.js";
 
 renderCheckoutHeader();
 
 renderOrderSummary();
 
 renderPaymentSummary();
+
