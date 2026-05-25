37607deb4be2f01f27521c194cdcc7bd97450648
Adarsh Ranjan <adarsh.aur@gmail.com>
2026-05-25 13:12:34 +0530

    Created oop version of cart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,25 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [
-        {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: '2'
-        }
-    ];
+loadFromStorage();
+
+export function loadFromStorage() {
+
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: '2'
+            }
+        ];
+    }
 }
 
 function saveStorage() {
@@ -69,11 +76,11 @@ export function updateCartQuantity() {
     saveStorage();
 }
 
-export function updateQuantity(productId, quantityValue){
+export function updateQuantity(productId, quantityValue) {
     let matchingItem;
 
     cart.forEach((item) => {
-        if (item.productId === productId){
+        if (item.productId === productId) {
             matchingItem = item;
         }
     });
@@ -84,14 +91,14 @@ export function updateQuantity(productId, quantityValue){
     saveStorage();
 }
 
-export function updateDeliveryOption(productId, deliveryOptionId){
+export function updateDeliveryOption(productId, deliveryOptionId) {
     let matchingItem;
     cart.forEach((item) => {
-        if (item.productId === productId){
+        if (item.productId === productId) {
             matchingItem = item;
         }
     });
-    
+
     matchingItem.deliveryOptionId = deliveryOptionId;
 
     saveStorage();
--- data/products.js
+++ data/products.js
@@ -6,6 +6,7 @@ export function getProduct(productId) {
       matchingProduct = product;
     }
   });
+  
   return matchingProduct;
 }
 
