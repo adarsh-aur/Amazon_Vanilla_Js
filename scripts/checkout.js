a2cb8c41bffc4f2e6d712cb707f9e3068172e14f
Adarsh Ranjan <adarsh.aur@gmail.com>
2026-05-26 00:04:00 +0530

    fetch and response used in checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "../scripts/checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-new Promise((resolve) => {
-    loadProducts(() => {
-        resolve();
-    });
-}).then(() => {
+loadProductsFetch().then(() => {
     renderCheckoutHeader();
-
     renderOrderSummary();
-
-    renderPaymentSummary();
+    renderPaymentSummary(); 
 });
